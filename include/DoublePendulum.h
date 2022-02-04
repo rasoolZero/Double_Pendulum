a68e1a8e4ad0b1f88ec577d2ef3034835675c7c7
rasoolZero <rasool.kamkar20@gmail.com>
2022-02-04 18:32:37 +0330

    added getters and setters to DoublePendulum class

--- include/DoublePendulum.h
+++ include/DoublePendulum.h
@@ -5,19 +5,108 @@
 
 class DoublePendulum{
 private:
-    float rodOneLength=100;
-    float rodTwoLength=100;
-    float massOne=10;
-    float massTwo=10;
-    float angle1=glm::half_pi<float>();
-    float angle2=glm::quarter_pi<float>();
+    float rodOneLength;
+    float rodTwoLength;
+    float massOne;
+    float massTwo;
+    float velocity1=0.0F;
+    float velocity2=0.0F;
+    float acc1;
+    float acc2;
+    float angle1;
+    float angle2;
+
+    float getRodOneLength() {
+    	return this->rodOneLength;
+    }
+    void setRodOneLength(float rodOneLength) {
+    	this->rodOneLength = rodOneLength;
+    }
+
+
+    float getRodTwoLength() {
+    	return this->rodTwoLength;
+    }
+    void setRodTwoLength(float rodTwoLength) {
+    	this->rodTwoLength = rodTwoLength;
+    }
+
+
+    float getMassOne() {
+    	return this->massOne;
+    }
+    void setMassOne(float massOne) {
+    	this->massOne = massOne;
+    }
+
+
+    float getMassTwo() {
+    	return this->massTwo;
+    }
+    void setMassTwo(float massTwo) {
+    	this->massTwo = massTwo;
+    }
+
+
+    float getVelocity1() {
+    	return this->velocity1;
+    }
+    void setVelocity1(float velocity1) {
+    	this->velocity1 = velocity1;
+    }
+
+
+    float getVelocity2() {
+    	return this->velocity2;
+    }
+    void setVelocity2(float velocity2) {
+    	this->velocity2 = velocity2;
+    }
+
+
+    float getAcc1() {
+    	return this->acc1;
+    }
+    void setAcc1(float acc1) {
+    	this->acc1 = acc1;
+    }
+
+
+    float getAcc2() {
+    	return this->acc2;
+    }
+    void setAcc2(float acc2) {
+    	this->acc2 = acc2;
+    }
+
+
+    float getAngle1() {
+    	return this->angle1;
+    }
+    void setAngle1(float angle1) {
+    	this->angle1 = angle1;
+    }
+
+
+    float getAngle2() {
+    	return this->angle2;
+    }
+    void setAngle2(float angle2) {
+    	this->angle2 = angle2;
+    }
+
+    const float g = 1.0F;
     ci::vec2 ball1,ball2;
     void updateBall1();
     void updateBall2();
+    void updateAcc1();
+    void updateAcc2();
+    void updateAngles();
     void drawPart1();
     void drawPart2();
 
 public:
+    DoublePendulum();
     void update();
     void draw();
 };
--- src/DoublePendulum.cpp
+++ src/DoublePendulum.cpp
@@ -1,7 +1,15 @@
 #include "DoublePendulum.h"
 #include "cinder/CinderGlm.h"
 #include "cinder/gl/gl.h"
+
+DoublePendulum::DoublePendulum(){
+    massOne = massTwo = 10;
+    rodOneLength = rodTwoLength = 100;
+    angle1 = angle2 = glm::half_pi<float>();
+}
 void DoublePendulum::update(){
+    updateAcc1();
+    updateAcc2();
     updateBall1();
     updateBall2();
 }
@@ -13,15 +21,52 @@ void DoublePendulum::updateBall2(){
     ball2.x = ball1.x + rodTwoLength * glm::sin(angle2);
     ball2.y = ball1.y + rodTwoLength * glm::cos(angle2);
 }
+void DoublePendulum::updateAcc1(){
+    const float m1=massOne;
+    const float m2=massTwo;
+    const float a1=angle1;
+    const float a2=angle2;
+    const float r1=rodOneLength;
+    const float r2=rodTwoLength;
+    float num1 = -g * (2 * m1 + m2) * sin(a1);
+    float num2 = -m2 * g * sin(a1-2*a2);
+    float num3 = -2*sin(a1-a2)*m2;
+    float num4 = velocity2*velocity2*r2+velocity1*velocity1*r1*cos(a1-a2);
+    float den = r1 * (2*m1+m2-m2*cos(2*a1-2*a2));
+    acc1 = (num1 + num2 + num3*num4) / den;
+}
+void DoublePendulum::updateAcc2(){
+    const float m1=massOne;
+    const float m2=massTwo;
+    const float a1=angle1;
+    const float a2=angle2;
+    const float r1=rodOneLength;
+    const float r2=rodTwoLength;
+    float num1 = 2 * sin(a1-a2);
+    float num2 = (velocity1*velocity1*r1*(m1+m2));
+    float num3 = g * (m1 + m2) * cos(a1);
+    float num4 = velocity2*velocity2*r2*m2*cos(a1-a2);
+    float den = r2 * (2*m1+m2-m2*cos(2*a1-2*a2));
+    acc2 = (num1*(num2+num3+num4)) / den;
+}
+void DoublePendulum::updateAngles(){
+    velocity1+=acc1;
+    angle1+=velocity1;
+    //velocity1*=0.99F;
+    velocity2+=acc2;
+    angle2+=velocity2;
+    //velocity2*=0.99F;
+}
 
 void DoublePendulum::draw(){
     ci::gl::lineWidth(3);
     drawPart1();
     drawPart2();
+    updateAngles();
 }
 
 void DoublePendulum::drawPart1(){
-    static ci::vec2 origin;
+    const static ci::vec2 origin;
     ci::gl::color(ci::Color(1,0,0));
     ci::gl::drawLine(origin,ball1);
     ci::gl::drawSolidCircle(ball1,massOne);
